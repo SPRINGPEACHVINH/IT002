0a9f45d2873c4cd77af9e3e3f5d10876ab91f5cf
unknown <daoxuan.vinh3@gmail.com>
2023-05-31 13:46:11 +0700

    commit

--- lab6/CString.cpp
+++ lab6/CString.cpp
@@ -1,4 +1,25 @@
-#include "CString.h"
+#include <iostream>
+using namespace std;
+class String
+{
+private:
+    char *s;
+
+public:
+    String();
+    String(char *);
+    ~String();
+    int length();
+    char *strcpy(char *, const char *);
+    int strlen(const char *);
+    char *strcat(char *, const char *);
+    String dao();
+    String noi(String);
+    String to_upper();
+    String to_lower();
+    friend istream &operator>>(istream &, String &);
+    friend ostream &operator<<(ostream &, const String &);
+};
 String::String()
 {
     s = new char[1];
