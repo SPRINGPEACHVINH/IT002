e55fa92d5bb7b0c4a51817cc4b8e1b1c7acc7502
SPRINGPEACHVINH <daoxuan.vinh3@gmail.com>
2023-07-21 22:56:07 +0700

    commit

---
+++ test.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+void insertion(int arr[], int n)
+{
+    int i, j, key;
+    for (int i = 0; i < n; i++)
+    {
+        key = arr[i];
+        j = i - 1;
+        while (j >= 0 && arr[j] > key)
+        {
+            int temp = arr[j];
+            arr[j + 1] = arr[j];
+            j--;
+        }
+    }
+    arr[j + 1] = key;
+}
+int main()
+{
+    int arr[4] = {2, 4, 1, 5};
+    insertion(arr, 4);
+    for (int i = 0; i < 4; i++)
+    {
+        cout << arr[i] << " " << endl;
+    }
+}
